23fa4655a65a9af70d6ce80e5415bf9bae0c93f9
Ahokas Maria Katariina <maria.ahokas@student.careeria.fi>
2019-08-17 16:16:47 +0300

    Lehtisivu valmis

--- Palvelukoira/Controllers/PalvelukoiratController.cs
+++ Palvelukoira/Controllers/PalvelukoiratController.cs
@@ -22,6 +22,11 @@ namespace Palvelukoira.Controllers
             return View();
         }
 
+        public ActionResult Lehti()
+        {
+            return View();
+        }
+
 
     }
 }
---
+++ Palvelukoira/Images/lehti.php
@@ -0,0 +1,139 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<title>Porvoon Palveluskoirat - Borg&aring; Brukshundar ry</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<meta name="Description" content=""/>
+<meta name="keywords" content="" />
+<link rel="stylesheet" type="text/css" href="ppk_style.css"/>
+</head>
+
+<body>
+	<!-- *** Header *** -->
+			<div id="header">
+				<!-- *** Logo *** -->
+				<div id="logo_left">
+				<img src="pics/logo5.jpg" alt="Porvoon Palveluskoirat - Borg&aring; Brukshundar ry">
+
+		    </div>
+
+
+
+<!-- *** End Menu ** -->
+			</div>
+
+<!-- *** Main wrapper **  -->
+<div id="wrapper">
+
+    <!-- *** Horizontal menu  *** -->
+<div id="centeredmenu">
+<?php include("navigation.php"); ?>
+</div>
+
+
+
+<!-- *** Start Content *** -->
+<div id="content">
+<h3>HAUKKUSET</h3>
+			<p>
+			<img border="0" src="lehti/408.jpg" width="150" height="212">&nbsp;&nbsp;&nbsp;&nbsp;
+			<img border="0" src="lehti/308.jpg" width="150" height="212">&nbsp;&nbsp;&nbsp;&nbsp;
+			<img border="0" src="lehti/208.jpg" width="150" height="212">&nbsp;&nbsp;&nbsp;&nbsp;
+			<img border="0" src="lehti/409.jpg" width="150" height="212">&nbsp;&nbsp;&nbsp;&nbsp;
+			<img border="0" src="lehti/108.jpg" width="150" height="212">
+			</p>
+<p>
+Haukkuset on Porvoon Palveluskoirat - Borg&aring; Brukshundar ry:n j&auml;senjulkaisu, joka sis&auml;lt&auml;&auml;
+artikkeleita palvelus- ja pelastuskoiratoiminnasta sek&auml; ajankohtaista tietoa yhdistyksen toiminnasta.
+Lehti ilmestyy kaksi kertaa vuodessa.</p>
+
+<h5 id="left">Ilmoitushinnat: </h5>
+ <p> 1/1 sivu 100 &euro;  ja ½ sivu 50 &euro;</p>
+<h5 id="left">Yhteystiedot:</h5>
+<p>
+lehti @ porvoonpalveluskoirat.fi
+</p>
+
+<h5 id="left">Lehtitoimikunta:</h5>
+<p>
+Marian Ahlskog-Karhu<br/>
+Christina Klawer-Kauranen<br/>
+Carita Forsberg-Heikkil&auml;<br/>
+Tere J&auml;&auml;skel&auml;inen<br/>
+Anna-Mari Matikainen<br/>
+<br/>
+
+ </p>
+ <p>
+Toimikunta ottaa mielell&auml;&auml;n vastaan koetuloksia, kuvia, kirjoituksia ja aiheita artikkeleihin.
+</p> <!--
+<table class="event">
+
+  <tr class="mon">
+     <td class="mot">Lehti nro</td>
+     <td class="mot">Ilmestymisviikko</td>
+     <td class="mot">Materiaalin j&auml;tt&ouml;p&auml;iv&auml;</td>
+  </tr>
+
+    <tr>
+     <td  class="act">1/2012</td>
+     <td  class="act">13</td>
+     <td  class="act">10.3.</td>
+  </tr>
+    <tr>
+     <td  class="act">2/2012</td>
+     <td  class="act">25</td>
+     <td  class="act">3.6.</td>
+  </tr>
+      <tr>
+     <td  class="act">3/2012</td>
+     <td  class="act">36</td>
+     <td  class="act">18.8. </td>
+  </tr>
+
+    <tr>
+     <td  class="act">4/2012</td>
+     <td  class="act">50</td>
+     <td  class="act">15.11.</td>
+  </tr>
+
+
+</table> -->
+
+<p>Tässä alla klikkaamalla kuvia voit lukea ihka ensimmäisen Haukkuset-lehden,joka ilmestyi vuonna 1985 ja vuoden 2016 ensimmäisen lehden pdf-versioina.
+</p>
+<p><br>			<center><a href="historia/hset1nettilehti.pdf"><img border="0" src="historia/haukkuset1kansip.gif" width="150" height="212">
+<a href="docs/Haukkuset_1_2016.pdf"><img border="0" src="docs/Haukkuset_1_2016.jpg" width="150" height="212"></a></center>
+			
+<p>Jos haluat luopua paperille painetusta lehdestä ja lukea Haukkuset-lehteäsi vain netistä, niin ilmoita sähköpostiosoitteesi sihteerille:  sihteeri@porvoonpalveluskoirat.fi
+</p>
+
+<hr/>
+
+			
+</p>
+
+
+</div>
+<!-- *** End Content *** -->
+
+
+<!-- *** Start footer *** -->
+<div id="footer">
+<div id="foot">
+<p>&copy; Porvoon Palveluskoirat - Borg&aring; Brukshundar ry |  <a href="yhteystiedot.php">Yhteystiedot</a> | <a href="paivitykset.php">P&auml;ivitykset</a></p>
+</div>
+</div>
+<!-- *** End footer *** -->
+
+
+
+</div> <!-- *** End wrapper *** -->
+
+
+
+
+
+</body>
+</html>
